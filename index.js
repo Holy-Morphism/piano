fb44ef2e1612fa3a5dec62283a6a0a30956e3780
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-08-09 02:56:31 +0500

    Loading screen removed

--- index.js
+++ index.js
@@ -4,16 +4,43 @@ for (let i = 0; i < noOfDrums; i++) {
   document.querySelectorAll(".pkey")[i].addEventListener("click", function () {
     var sound = new Audio("sounds/key" + (i + 1).toString() + ".mp3");
     sound.play();
-    this.style.color = "red";
+    var buttonPressed = this;
+    buttonPressed.classList.add("pressed");
+
+    setTimeout(function () {
+      buttonPressed.classList.remove("pressed");
+    }, 100);
   });
 }
 
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-    document.querySelector("body").style.visibility = "hidden";
-    document.querySelector("#loader").style.visibility = "visible";
-  } else {
-    document.querySelector("#loader").style.display = "none";
-    document.querySelector("body").style.visibility = "visible";
+document.addEventListener("keypress", function (event) {
+  var sound;
+  switch (event.key) {
+    case "q":
+      sound = new Audio("sounds/key1.mp3");
+      break;
+    case "w":
+      sound = new Audio("sounds/key2.mp3");
+      break;
+    case "e":
+      sound = new Audio("sounds/key3.mp3");
+      break;
+    case "r":
+      sound = new Audio("sounds/key4.mp3");
+      break;
+    case "t":
+      sound = new Audio("sounds/key5.mp3");
+      break;
+    case "y":
+      sound = new Audio("sounds/key6.mp3");
+      break;
+    default:
+      break;
   }
-};
+  sound.play();
+  var buttonPressed = document.getElementById(event.key).classList;
+  buttonPressed.add("pressed");
+  setTimeout(function () {
+    buttonPressed.remove("pressed");
+  }, 100);
+});
